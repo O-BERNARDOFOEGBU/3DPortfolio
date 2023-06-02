c52265d4a849b1911300685ab27694591ccb62ae
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2023-06-02 08:27:08 +0100

    testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -207,7 +207,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      "I thought it was impossible to make a website as beautiful as our product, but Bernard proved me wrong.",
     name: "Asimi Olufisayo",
     designation: "Legal and Compliance Officer",
     company: "Landmark Africa",
